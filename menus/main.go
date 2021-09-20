fa845da308f6dcb59cd89bd470e33358f5a09afd
Lea Anthony <lea.anthony@gmail.com>
2021-09-20 21:29:06 +1000

    Tidy up

--- menus/main.go
+++ menus/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"embed"
-	"github.com/wailsapp/wails/v2/pkg/options/windows"
 	"log"
 
 	"github.com/wailsapp/wails/v2"
@@ -19,30 +18,16 @@ func main() {
 	app := NewApp()
 
 	err := wails.Run(&options.App{
-		Title:             "Menus Demo",
-		Width:             800,
-		Height:            600,
-		MinWidth:          400,
-		MinHeight:         400,
-		MaxWidth:          1280,
-		MaxHeight:         1024,
-		DisableResize:     false,
-		Fullscreen:        false,
-		Frameless:         false,
-		StartHidden:       false,
-		HideWindowOnClose: false,
-		RGBA: &options.RGBA{
-			R: 0,
-			G: 0,
-			B: 0,
-			A: 255,
-		},
-		Menu:   app.applicationMenu(),
-		Assets: assets,
-		Windows: &windows.Options{
-			//WebviewIsTransparent: true,
-			WindowIsTranslucent: false,
-		},
+		Title:      "Menus Demo",
+		Width:      800,
+		Height:     600,
+		MinWidth:   400,
+		MinHeight:  400,
+		MaxWidth:   1280,
+		MaxHeight:  1024,
+		RGBA:       &options.RGBA{R: 0, G: 0, B: 0, A: 255},
+		Menu:       app.applicationMenu(),
+		Assets:     assets,
 		LogLevel:   logger.DEBUG,
 		OnStartup:  app.startup,
 		OnShutdown: app.shutdown,
